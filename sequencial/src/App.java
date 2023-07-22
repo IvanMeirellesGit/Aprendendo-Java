3944ef0f8db7c2cc31799932e27d41218f9d14e6
Ivan Meirelles <hackermeirelles+github@gmail.com>
2023-07-22 09:14:18 -0300

    sequencial

---
+++ sequencial/src/App.java
@@ -0,0 +1,12 @@
+class App {
+    public static void main(String[] args) {
+        double base, altura, area;
+
+        base = 20.0;
+        altura = 10.0;
+
+        area = (base * altura) / 2;
+
+        System.out.println(area);
+    }
+}
