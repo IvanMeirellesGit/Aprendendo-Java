bf9b17f7841e1f12c5028fbacc22fa2df6c13569
Ivan Meirelles <hackermeirelles+github@gmail.com>
2023-07-22 09:39:55 -0300

    Estrutura Condicional Composta

---
+++ estruturaCondicionalComposta/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+  public static void main(String[] args) {
+    int nota = 6;
+
+    if (nota >= 5) {
+      System.out.println("Aprovado!");
+    } else {
+      System.out.println("Reprovado!");
+    }
+  }
+}
