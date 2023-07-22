212e1f186dc6cc62417b201367813c67c4b03778
Ivan Meirelles <hackermeirelles+github@gmail.com>
2023-07-22 09:32:59 -0300

    Operadores Lógicos

---
+++ logicos/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 8;
+        b = 4;
+
+        System.out.println(b > a);
+
+        System.out.println(a > b || b == 8);
+    }
+}
