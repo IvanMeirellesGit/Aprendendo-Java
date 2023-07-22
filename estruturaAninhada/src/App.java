2d3c51d76bab67e89167e5b22c03ad81c4650687
Ivan Meirelles <hackermeirelles+github@gmail.com>
2023-07-22 09:55:56 -0300

    Condicional Aninhada

---
+++ estruturaAninhada/src/App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        int nota = 4;
+
+        if (nota >= 5) {
+            System.out.println("Aprovado!");
+        } else if (nota >= 3 && nota < 5) {
+            System.out.println("Exame");
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
+}
