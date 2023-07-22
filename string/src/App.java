d3d5f63b4b8344a1ebf675016f307c6247725156
Ivan Meirelles <hackermeirelles+github@gmail.com>
2023-07-22 09:17:46 -0300

    Aprendendo String

--- string/src/App.java
+++ string/src/App.java
@@ -1,5 +1,13 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+class App {
+    public static void main(String[] args) {
+        String valor = "Descomplica - Java";
+
+        System.out.println(valor);
+        System.out.println(valor.contains("Java"));
+        System.out.println(valor.length());
+
+        System.out.println("Com o println existe quebra de linha");
+        System.out.print("Com o print normal nao tem quebra de linha");
+        System.out.printf("Com o printf tambêm não existe quebra de linha.");
     }
 }
