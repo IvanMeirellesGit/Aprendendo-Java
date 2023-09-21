f963e4d22a4474796a8ed5f7fdfecc2566aff3c4
Ivan Meirelles <hackermeirelles+github@gmail.com>
2023-09-20 22:36:40 -0300

    Contando Passaros
    Usando vetor e usando for para percorrer e mostrar resultados

--- exercicio-vetor/src/App.java
+++ exercicio-vetor/src/App.java
@@ -1,5 +1,27 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int[] passarosPorDia = { 2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1 };
+
+        int totalPassaros = 0;
+        int passarosPrimeiraSemana = 0;
+        int passarosSegundaSemana = 0;
+
+        for (int i = 0; i < passarosPorDia.length; i++) {
+            totalPassaros = totalPassaros + passarosPorDia[i];
+        }
+
+        System.out.println(totalPassaros);
+
+        for (int i = 0; i < 7; i++) {
+            passarosPrimeiraSemana = passarosPrimeiraSemana + passarosPorDia[i];
+        }
+
+        System.out.println(passarosPrimeiraSemana);
+
+        for (int i = 7; i < passarosPorDia.length; i++) {
+            passarosSegundaSemana = passarosSegundaSemana + passarosPorDia[i];
+        }
+
+        System.out.println(passarosSegundaSemana);
     }
 }
