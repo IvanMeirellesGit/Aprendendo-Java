a06746d3279400ce4d816c53221d0cc8ff436ecb
Ivan Meirelles <hackermeirelles+github@gmail.com>
2023-09-20 22:19:50 -0300

    Matriz

--- aulaVetor/src/App.java
+++ aulaVetor/src/App.java
@@ -1,5 +1,12 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+class App {
+    public static void main(String[] args){
+        int valor;
+        int[] dados = new int[5];
+
+        dados[2] = 9;
+        dados[3] = 7;
+        dados[0] = 6;
+
+        System.out.println(dados[0]);
     }
 }
