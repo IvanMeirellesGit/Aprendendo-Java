f14d65716530b482c6cdb37fc6f5d8c0cee588e9
Ivan Meirelles <hackermeirelles+github@gmail.com>
2023-09-21 17:12:54 -0300

    ArrayList Completo

--- exercicio-ArrayList/src/App.java
+++ exercicio-ArrayList/src/App.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Scanner entradabanda = new Scanner(System.in);
+        Scanner quantidadeBanda = new Scanner(System.in);
+
+        ArrayList<String> bandas = new ArrayList<String>();
+
+        System.out.println("Quantas bandas quer adicionar?");
+
+        int valor = quantidadeBanda.nextInt();
+
+        if (valor != 0) {
+            for (int i = 0; i < valor; i++)
+                bandas.add(entradabanda.nextLine());
+        }
+
+        while (!bandas.isEmpty()) {
+            System.out.println(bandas.remove(0));
+        }
     }
 }
