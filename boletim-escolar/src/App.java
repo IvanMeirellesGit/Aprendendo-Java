8ce3b5485653b43734e98e4011b10b1ec999e30b
Ivan Meirelles <hackermeirelles+github@gmail.com>
2023-09-22 11:21:27 -0300

    exercicios de vetor

--- boletim-escolar/src/App.java
+++ boletim-escolar/src/App.java
@@ -1,5 +1,17 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        double[] notasCarlos = new double[4];
+
+        double media;
+
+        notasCarlos[0] = 4;
+        notasCarlos[1] = 8;
+        notasCarlos[2] = 6;
+        notasCarlos[3] = 8;
+
+        media = (notasCarlos[0] + notasCarlos[1] + notasCarlos[2] + notasCarlos[3]) / 4;
+
+        System.out.println("A mádia de notas do Carlos é: " + media);
     }
 }
---
+++ vetor-idades-alunos/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+
+        int idade[] = { 16, 18, 14, 19, 22, 20, 16, 15, 17, 19 };
+
+        for (int i = 0; i < 10; i++)
+            System.out.println("Idade: " + idade[i]);
+    }
+}
