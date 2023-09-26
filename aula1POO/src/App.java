a082dab7e5e64d615af2bc5f15fa11f6646cc252
Ivan Meirelles <hackermeirelles+github@gmail.com>
2023-09-26 09:26:47 -0300

    POO

---
+++ aula1POO/src/App.java
@@ -0,0 +1,10 @@
+class App {
+    public static void main(String[] args) {
+
+        Cao cachorro = new Cao();
+
+        cachorro.nome = "Bobby";
+        System.out.println(cachorro.nome);
+        cachorro.Anda();
+    }
+}
---
+++ aula1POO/src/Cao.java
@@ -0,0 +1,23 @@
+public class Cao {
+
+    String nome;
+    String cor;
+    int idade;
+    double peso;
+
+    public void Anda() {
+        System.out.println("Estou andando...");
+    }
+
+    public void Late() {
+        System.out.println("Estou latindo...");
+    }
+
+    public void Dorme() {
+        System.out.println("Dormindo...");
+    }
+
+    public void PegarOsso() {
+        System.out.println("Peguei Osso...");
+    }
+}
