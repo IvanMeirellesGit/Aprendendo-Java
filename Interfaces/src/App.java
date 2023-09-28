7deed7387901e0fe4eb80e9854d2ab8e95029ecc
Ivan Meirelles <hackermeirelles+github@gmail.com>
2023-09-28 20:41:58 -0300

    Update App.java

--- Interfaces/src/App.java
+++ Interfaces/src/App.java
@@ -2,6 +2,10 @@ public class App {
     public static void main(String[] args) {
 
         Conta cp = new Poupanca();
+        cp.depositar(850);
+        cp.sacar(12);
+
+        System.out.println(cp.getSaldo());
 
     }
 }
